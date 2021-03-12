ecbc7aa6e9d0dc03ef0665fc8416db3746e22df5
fargalaxy <far.galaxy.klg@gmail.com>
2021-03-12 13:26:39 +0400

    added user data in bot backend

--- questbot.py
+++ questbot.py
@@ -2,12 +2,21 @@
 import telebot;
 import traceback
 import json
+from sys import argv
+
+print("Starting...")
+
+try:
+    quest_file = argv[1]
+except Exception:
+    print("Error: bot doesn't started: quest file not definited")
+    raise 
 
 # Reading a token and launching a bot. 
 try:
     token = open("token.txt", 'r').readline().replace("\n","")
     bot = telebot.TeleBot(token);
-except:
+except Exception:
     print("Error: bot doesn't started. Check token in token.txt")
     raise
 
@@ -22,15 +31,47 @@ else:
 # Reading a quest.
 f = open('quest.json', 'r', encoding="utf-8")
 quest = json.loads(f.read())
-print(quest)
 f.close()
 
-tag = "start"
+class User():
+    def __init__(self, uid, tag):
+        self.uid = uid
+        self.tag = tag
+        self.data = {}
+try:        
+    f = open('users.json', 'r', encoding="utf-8")
+except FileNotFoundError:
+    f = open('users.json', 'w', encoding="utf-8")
+    f.write("{}")
+    f.close()
+    f = open('users.json', 'r', encoding="utf-8")
+    
+users_data = json.loads(f.read())
+f.close()
+
+users = {}
+for user in users_data:
+    users[user] = User(user, users_data[user]["tag"])
+
+def save_data():
+    f = open("users.json", 'w', encoding="utf-8")
+    data = {}
+    for user in users:
+        data[str(user)] = {"tag" : users[user].tag}
+    f.write(str(data).replace("'", '"'))    
 
 # /start message
 @bot.message_handler(commands=['start'])
 def hello(message):
+    global users
+    uid = message.from_user.id
+    
+    if not uid in users:
+        users[uid] = User(uid, "start")
+        
     bot.send_message(message.from_user.id, quest["start"]["bot_answer"])
+    users[uid].tag = "start"
+    save_data()
 
 # /help message    
 @bot.message_handler(commands=['help'])
@@ -40,16 +81,21 @@ def help(message):
 # Answers to questions and transitions in dialogue.
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    global tag
-    print(message)
+    global users    
+    uid = message.from_user.id
+    tag = users[uid].tag
+    print(uid, message.text)
     
-    if message.text.lower() in quest[tag]["goto"]:
+    if uid in users and message.text.lower() in [txt.lower() for txt in quest[tag]["goto"]]:
         tag = quest[tag]["goto"][message.text.lower()]
         bot.send_message(message.from_user.id, quest[tag]["bot_answer"])
+        users[uid].tag = tag
+        save_data()
         
     else:
         bot.send_message(message.from_user.id, quest["unidentified"])
         
 if __name__ == '__main__':
+    print("Bot running...")
     bot.polling(none_stop=True, interval=0)
     #bot.infinity_polling()
